92f78f4442f4d956d9aa4ae919b5217b72cab2e2
BaoChunhui <604176850@qq.com>
2022-10-06 19:03:08 +0800

    gcc 9.4.0 x86_64-linux-gnu编译得到的build文件
    gcc 9.4.0 x86_64-linux-gnu编译得到的build文件

---
+++ build/CMakeFiles/yd_demo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/GitHub repository/YDApi_Demo_1_188_78_0/src/YdUserApiInfo.cpp" "/mnt/d/GitHub repository/YDApi_Demo_1_188_78_0/build/CMakeFiles/yd_demo.dir/src/YdUserApiInfo.cpp.o"
+  "/mnt/d/GitHub repository/YDApi_Demo_1_188_78_0/src/main.cpp" "/mnt/d/GitHub repository/YDApi_Demo_1_188_78_0/build/CMakeFiles/yd_demo.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yd_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yd_demo.dir/src/YdUserApiInfo.cpp.o"
+  "CMakeFiles/yd_demo.dir/src/main.cpp.o"
+  "yd_demo"
+  "yd_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yd_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
